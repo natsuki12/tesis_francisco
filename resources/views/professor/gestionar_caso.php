09ceda93fd422c0a07dad67f4a225ac1e1494652
Francisco <fdiaz.6650@unimar.edu.ve>
2026-04-03 01:23:42 -0400

    UI: Ocultar botón 'Caso Publicado' (candado gris) para limpiar redundancia visual en cabecera de reporte

--- resources/views/professor/gestionar_caso.php
+++ resources/views/professor/gestionar_caso.php
@@ -230,15 +230,6 @@ if ($source === 'borrador') {
                     </svg>
                     Reactivar Caso
                 </button>
-            <?php else: ?>
-                <span class="btn btn-secondary" style="opacity:0.6;cursor:not-allowed;">
-                    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round">
-                        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
-                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
-                    </svg>
-                    Caso Publicado
-                </span>
             <?php endif; ?>
         </div>
     </div>
